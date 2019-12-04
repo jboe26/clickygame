d30caa8addebbf7ff82f21004adf3e5b7b70edc5
jboe26 <joshrboepple@outlook.com>
2019-12-04 18:10:23 -0500

    Updates

---
+++ precache-manifest.004ec01cbfb32a22ca5880b178e6626e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f405719c9e5909290ef1ab3515fe9836",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "d246515e57228e01ef09",
+    "url": "/clickygame/static/css/main.52caf570.chunk.css"
+  },
+  {
+    "revision": "33c2309a6b498767dd8c",
+    "url": "/clickygame/static/js/2.3907fedb.chunk.js"
+  },
+  {
+    "revision": "d246515e57228e01ef09",
+    "url": "/clickygame/static/js/main.c4206f8e.chunk.js"
+  },
+  {
+    "revision": "c886b7be9a5b824e2367",
+    "url": "/clickygame/static/js/runtime-main.24884677.js"
+  },
+  {
+    "revision": "ca0c356bd42c36fcf994f7be17074d9a",
+    "url": "/clickygame/static/media/cat10.ca0c356b.jpg"
+  },
+  {
+    "revision": "22200a47b5d9b8302455837e1a2256af",
+    "url": "/clickygame/static/media/dog1.22200a47.jpg"
+  },
+  {
+    "revision": "44c9d8980e663cfba5270e4b0b9c182c",
+    "url": "/clickygame/static/media/dog2.44c9d898.jpg"
+  },
+  {
+    "revision": "8434ea4826ef74f55e329565a0b62ae7",
+    "url": "/clickygame/static/media/dog3.8434ea48.jpg"
+  },
+  {
+    "revision": "31a9bce6918d2e5948e39015d5429763",
+    "url": "/clickygame/static/media/dog4.31a9bce6.jpg"
+  },
+  {
+    "revision": "6218082ba094c6d1985f0528c5208ba8",
+    "url": "/clickygame/static/media/dog5.6218082b.jpg"
+  },
+  {
+    "revision": "d15f9dbfaf83172ef10458ff03c92503",
+    "url": "/clickygame/static/media/dog6.d15f9dbf.jpg"
+  },
+  {
+    "revision": "b8297d4e7bf7ea81b9e574764e667f71",
+    "url": "/clickygame/static/media/dog7.b8297d4e.jpg"
+  },
+  {
+    "revision": "391aecada0429c4ed1bfc86cc335c0fd",
+    "url": "/clickygame/static/media/dog8.391aecad.jpg"
+  },
+  {
+    "revision": "a5e7fab4500ed24f67fd1f887d6d2979",
+    "url": "/clickygame/static/media/dog9.a5e7fab4.jpg"
+  }
+]);
